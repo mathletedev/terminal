ac46b8bb0eaf043d7c2060b5f6afcd5e1567e000
Neo Delta <mathletedev@gmail.com>
2022-05-01 00:34:17 -0700

    keyboard input

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,55 @@
-import { __colors__ } from "./lib/constants";
+import { __colors__, __cursorDelay__ } from "./lib/constants";
 
 let root = document.querySelector("#root")!;
 let line = document.querySelector("#line")!;
+let command = document.querySelector("#command") as HTMLSpanElement;
 let cursor = document.querySelector("#cursor") as HTMLSpanElement;
 
+let wait = 0;
+
 const echo = (text?: string, color?: string) => {
-	if (!text) {
-		root.insertBefore(document.createElement("br"), line);
-		return;
-	}
+  if (!text) {
+    root.insertBefore(document.createElement("br"), line);
+    return;
+  }
 
-	let span = document.createElement("span");
-	span.innerHTML = text;
-	span.style.color = color || __colors__.white;
+  let span = document.createElement("span");
+  span.innerHTML = text;
+  span.style.color = color || __colors__.white;
 
-	root.insertBefore(span, line);
+  root.insertBefore(span, line);
 };
 
 echo("Hello, ", __colors__.red);
 echo("world!", __colors__.green);
 echo();
 echo("MathleteDev");
+echo();
+
+window.addEventListener("keydown", (ev) => {
+  wait = Date.now();
+
+  if (ev.key === "Enter") {
+    echo(command.innerHTML, __colors__.green);
+    echo();
+    return (command.innerHTML = "");
+  }
+
+  if (ev.key === "Backspace")
+    return (command.innerHTML = command.innerHTML.substring(
+      0,
+      command.innerHTML.length - 1
+    ));
+  if (ev.key.length === 1) command.innerHTML += ev.key;
+});
 
 const loop = () => {
-	requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 
-	cursor.style.height = `${
-		23 - Math.pow(Math.sin(Date.now() / 300), 4) * 23
-	}px`;
+  cursor.style.height =
+    wait + __cursorDelay__ > Date.now()
+      ? "23px"
+      : `${23 - Math.pow(Math.sin(Date.now() / 300), 4) * 23}px`;
 };
 
 requestAnimationFrame(loop);
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,11 @@
 export const __colors__ = {
-	black: "#282c34",
-	white: "#dcdfe4",
-	red: "#e06c75",
-	yellow: "#e5c07b",
-	green: "#98c379",
-	cyan: "#56b6c2",
-	blue: "#61afef",
-	magenta: "#c678dd"
+  black: "#282c34",
+  white: "#dcdfe4",
+  red: "#e06c75",
+  yellow: "#e5c07b",
+  green: "#98c379",
+  cyan: "#56b6c2",
+  blue: "#61afef",
+  magenta: "#c678dd",
 };
+export const __cursorDelay__ = 500;
