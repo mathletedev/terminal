693d498e591b5136d171546482bcd95f82a2d153
Neo Delta <mathletedev@gmail.com>
2022-05-01 12:40:26 -0700

    better input

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,24 @@
-import Keyboard from "simple-keyboard";
 import { __colors__, __cursorDelay__ } from "./lib/constants";
 
-let root = document.querySelector("#root") as HTMLElement;
+let root = document.querySelector("#root")!;
 let line = document.querySelector("#line")!;
-let command = document.querySelector("#command") as HTMLSpanElement;
+let input = document.querySelector("input") as HTMLInputElement;
 let cursor = document.querySelector("#cursor") as HTMLSpanElement;
 
 let wait = 0;
 
-if (/Mobi/.test(navigator.userAgent)) {
-	let elem = document.createElement("div");
-	elem.className = "simple-keyboard";
-	document.body.appendChild(elem);
+window.onkeydown = ({ key }) => {
+	input.focus();
+	wait = Date.now();
 
-	new Keyboard({
-		onKeyPress: (key: string) => {
-			wait = Date.now();
+	if (key === "Enter") {
+		echo(input.value);
+		echo();
+		input.value = "";
+	}
+};
 
-			switch (key) {
-				default:
-					command.innerHTML += key;
-					break;
-				case "=>":
-					exec();
-					break;
-				case "<=":
-					command.innerHTML = command.innerHTML.substring(
-						0,
-						command.innerHTML.length - 1
-					);
-					break;
-				case "{space}":
-					command.innerHTML += " ";
-					break;
-			}
-		},
-		layout: {
-			default: [
-				"q w e r t y u i o p",
-				"a s d f g h j k l",
-				"<= z x c v b n m =>",
-				"{space}"
-			]
-		}
-	});
-
-	root.style.height = `calc(100vh - ${elem.clientHeight}px)`;
-}
+input.onblur = () => input.focus();
 
 const echo = (text?: string, color?: string) => {
 	if (!text) {
@@ -61,30 +33,6 @@ const echo = (text?: string, color?: string) => {
 	root.insertBefore(span, line);
 };
 
-const exec = () => {
-	echo(command.innerHTML, __colors__.green);
-	echo();
-	command.innerHTML = "";
-};
-
-echo("Hello, ", __colors__.red);
-echo("world!", __colors__.green);
-echo();
-echo("MathleteDev");
-echo();
-
-window.addEventListener("keydown", (ev) => {
-	wait = Date.now();
-
-	if (ev.key === "Enter") return exec();
-	if (ev.key === "Backspace")
-		return (command.innerHTML = command.innerHTML.substring(
-			0,
-			command.innerHTML.length - 1
-		));
-	if (ev.key.length === 1) command.innerHTML += ev.key;
-});
-
 const loop = () => {
 	requestAnimationFrame(loop);
 
@@ -92,6 +40,10 @@ const loop = () => {
 		wait + __cursorDelay__ > Date.now()
 			? "23px"
 			: `${23 - Math.pow(Math.sin(Date.now() / 300), 4) * 23}px`;
+
+	cursor.style.left = `${
+		-window.innerWidth + 22 + (input.selectionStart! + 1) * 10.55
+	}px`;
 };
 
 requestAnimationFrame(loop);
