d1578814a1e1a13e888494dda4435065f9b80cfe
Neo Delta <mathletedev@gmail.com>
2022-05-08 14:31:03 -0700

    ls update command list

--- src/commands/ls.ts
+++ src/commands/ls.ts
@@ -2,6 +2,6 @@ import { __colors__ } from "../lib/constants";
 import { Command } from "../lib/types";
 
 export default ((term) => {
-	term.echoLn("clear  echo  help  ls  neofetch", __colors__.blue);
+	term.echoLn("clear  echo  help  ls  neofetch skills yes", __colors__.blue);
 	term.echo();
 }) as Command;
